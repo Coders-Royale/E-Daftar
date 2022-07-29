cf50a7fb8f83917aeef25fab2611576b453085c8
RaghhavDTurki <raghhavdturki@gmail.com>
2022-07-29 19:18:14 +0530

    chore: Lint file

--- server/src/utils/genPassword.ts
+++ server/src/utils/genPassword.ts
@@ -4,7 +4,7 @@ const passwordLength = 7;
 export function genPassword() {
     let password = "";
     for (let i = 0; i < passwordLength; i++) {
-        let randomNumber = Math.floor(Math.random() * chars.length);
+        const randomNumber = Math.floor(Math.random() * chars.length);
         password += chars.substring(randomNumber, randomNumber + 1);
     }
     return password;
