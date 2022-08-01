a05cdc55a1c07dccad71ad1dfdbe8fa6bbb44da9
VinVash <2020kucp1046@iiitkota.ac.in>
2022-08-01 14:58:08 +0530

    fix: socket error

--- client/src/helpers/socket.js
+++ client/src/helpers/socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 
-const SOCKET_API = "your-socket-api";
+const SOCKET_API = "https://sih-email.herokuapp.com";
 
 const getSocket = () => {
     return io(SOCKET_API);
--- client/src/pages/user/Sent.tsx
+++ client/src/pages/user/Sent.tsx
@@ -17,9 +17,9 @@ import Email2 from "../../images/tracking_page_email_2.png";
 import Email3 from "../../images/tracking_page_email_3.png";
 import Dp from "../../images/profile_page_dp.png";
 
-import getSocket from "../../helpers/socket";
+// import getSocket from "../../helpers/socket";
 
-const socket = getSocket();
+// const socket = getSocket();
 
 enum Status {
   Pending = "Pending",
