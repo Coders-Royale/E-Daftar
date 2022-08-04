2e77d2b94db8deeca9fb3b27c56bd2b35f7ac4b1
HimanshuChittora23908 <2020kucp1023@iiitkota.ac.in>
2022-08-04 10:26:53 +0530

    api hooks added

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,32 +1,81 @@
 const baseUrl = "https://sih-2022-server.azurewebsites.net/api";
 
 const apiEndPoints = {
+  // GET REQUESTS
+  getEmployee: `/getEmployee`,
+  trackStatus: `/trackStatus`,
+  listDocument: `/listDocument`,
+
+  // POST REQUESTS
   login: `/login`,
   createEmployee: `/createEmployee`,
-  getEmployee: `/getEmployee`,
+  createDocument: `/createDocument`,
+  assignDocument: `/assignDocument`,
+  forwardToAdmin: `/forwardToAdmin`,
+  rejectDocument: `/rejectDocument`,
 };
 
-// export async function getStudentDetails(params) {
-//   try {
-//     const res = (
-//       await fetch(
-//         baseUrl + apiEndPoints.getStudentDetails + `?studentid=${params.studentid}`,
-//         {
-//           headers: {
-//             Authorization: `Bearer ${localStorage.getItem(
-//               localStorageKey.jwtToken
-//             )}`,
-//             "Content-type": "application/json",
-//           },
-//         }
-//       ).then((res) => res.json())
-//     );
-//     return res;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
+// GET REQUESTS
+export async function getEmployee(params) {
+  try {
+    const res = (
+      await fetch(
+        baseUrl + apiEndPoints.getEmployee + `?departmentId=${params.departmentId}` + `&employeeId=${params.employeeId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
+            "Content-type": "application/json",
+          },
+        }
+      ).then((res) => res.json())
+    );
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
+export async function trackStatus(params) {
+  try {
+    const res = (
+      await fetch(
+        baseUrl + apiEndPoints.trackStatus + `?documentId=${params.documentId} ` + `&employeeId=${params.employeeId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
+            "Content-type": "application/json",
+          },
+        }
+      ).then((res) => res.json())
+    );
+    return res;
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+export async function listDocument(params) {
+  try {
+    const res = (
+      await fetch(
+        baseUrl + apiEndPoints.listDocument + `?employeeId=${params.employeeId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
+            "Content-type": "application/json",
+          },
+        }
+      ).then((res) => res.json())
+    );
+    return res;
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+// POST REQUESTS
 export async function login(data) {
   try {
     const res = (
@@ -62,18 +111,72 @@ export async function createEmployee(data) {
   }
 }
 
-export async function getEmployee(params) {
-	try {
+export async function createDocument(data) {
+  try {
     const res = (
-      await fetch(
-        baseUrl + apiEndPoints.getEmployee + `?departmentId=${params.departmentId}` + `&employeeId=${params.employeeId}`,
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
-            "Content-type": "application/json",
-          },
-        }
-      ).then((res) => res.json())
+      await fetch(baseUrl + apiEndPoints.createDocument, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }).then((res) => res.json())
+    );
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function assignDocument(data) {
+  try {
+    const res = (
+      await fetch(baseUrl + apiEndPoints.assignDocument, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }).then((res) => res.json())
+    );
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function forwardToAdmin(data) {
+  try {
+    const res = (
+      await fetch(baseUrl + apiEndPoints.forwardToAdmin, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }).then((res) => res.json())
+    );
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+export async function rejectDocument(data) {
+  try {
+    const res = (
+      await fetch(baseUrl + apiEndPoints.rejectDocument, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("jwtToken")}`,
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }).then((res) => res.json())
     );
     return res;
   } catch (error) {
