408f0931c6aa3eedc959e3dbbcb89b61e0195b1d
VinVash <2020kucp1046@iiitkota.ac.in>
2022-08-25 18:26:35 +0530

    fix: final commit before first build

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "./themes/ThemeContext";
 import Background from "./components/Background";
-
+import Toggle from "./components/ThemeToggle";
 import "./styles/index.css";
 import App from "./App";
 
@@ -13,7 +13,10 @@ root.render(
   <BrowserRouter>
     <ThemeProvider initialTheme="light">
       <Background>
-        {window.location.pathname !== "/" ? <App /> : null}
+        <div className="absolute right-0 top-0 mr-8 mt-2">
+          <Toggle />
+        </div>
+        <App />
       </Background>
     </ThemeProvider>
   </BrowserRouter>
